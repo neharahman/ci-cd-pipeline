54e6e2350c6b98572d8a77af0cc38608457eb2e8
NEHA RAHMAN <neha@Nehas-MacBook-Air.local>
2023-09-29 18:14:21 +0530

    cicd

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.get('/',(req,res)=>{
 app.get('/home',(req,res)=>{
     res.send('hello home')
 })
-
+app.get('/cicd',(req,res)=>{
+    res.send('hello ci cd auto deployed successfully hi')
+})
 
 app.listen(process.env.PORT,()=>{
     console.log('listening to the server')
